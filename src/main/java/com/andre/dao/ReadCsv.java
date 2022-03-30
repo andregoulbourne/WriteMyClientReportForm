0995a7929918df81e3025d328a2161f9573793d2
andregoulbourne <andre.goulbourne2@gmail.com>
2022-03-29 21:39:53 -0400

    Project-1: Put in a logger

--- src/main/java/com/andre/dao/ReadCsv.java
+++ src/main/java/com/andre/dao/ReadCsv.java
@@ -5,19 +5,22 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
+import org.apache.log4j.Logger;
 
 import com.andre.model.SummaryObjectVO;
 
 public class ReadCsv {
+	
+	private static final Logger logger = Logger.getLogger(ReadCsv.class);
 	
 	private String fileInPath;
 	
 	public void setFileInPath(String fileInPath) {
 		this.fileInPath = fileInPath;
 	}
-
+	
 	public List<SummaryObjectVO> readCSVFile() {
+		
 		try (var br = new BufferedReader( new FileReader(fileInPath))) {
 			var rs = new ArrayList<SummaryObjectVO>();
 			
@@ -25,7 +28,7 @@ public class ReadCsv {
 			var firstRow = true;
 			while((sCurrentLine = br.readLine()) != null) {
 				if(!firstRow) {
-				System.out.println(sCurrentLine);
+				logger.info(String.format("current Line in readCSVFile Is ... %s",sCurrentLine));
 				
 				var valuesList = split(sCurrentLine);
 				
@@ -38,7 +41,7 @@ public class ReadCsv {
 			}
 			return rs;
 		} catch(Exception e) {
-			System.out.println("An Exception occured "+ e);
+			logger.error("An Exception occured ...", e);
 		}
 		return new ArrayList<>();
 	}
@@ -48,20 +51,22 @@ public class ReadCsv {
 		var valuesList = new ArrayList<String>();
 		var regular = true;
 		for(var i=0; i<in.length(); i++) {
-			if(in.charAt(i)!=',' && in.charAt(i)!='\"' && regular)  storedResult.append(in.charAt(i));
-			else if(in.charAt(i)!='\"' && !regular)  storedResult.append(in.charAt(i));
+			if((in.charAt(i)!=',' && in.charAt(i)!='\"' && regular) || 
+			(in.charAt(i)!='\"' && !regular))  storedResult.append(in.charAt(i));
 			if(in.charAt(i)=='\"') regular = !regular;
 			if((in.charAt(i)==',' || i==in.length()-1) && regular) {
 				valuesList.add(storedResult.toString());
 				storedResult = new StringBuilder();
 			}
 		}
-		System.out.println(valuesList);
+		logger.info(String.format("valueList Once splited is ... %s",valuesList.toString()));
 		return valuesList;
 	}
 	
+	private SummaryObjectVO summaryObject;
+	
 	private SummaryObjectVO listToObject(List<String> list) {
-		var summaryObject = new SummaryObjectVO();
+		summaryObject = new SummaryObjectVO();
 		for(var i=0; i<list.size(); i++) {
 			var a = i+1;
 			if(a==1) {
@@ -70,19 +75,26 @@ public class ReadCsv {
 			} else if(a==2) {
 				summaryObject.setStatus(list.get(i));
 			} else if(a==3) {
-				if(list.get(i).equals("1")) summaryObject.setMadeADifference(true);
-				if(list.get(i).equals("0")) summaryObject.setMadeADifference(false);
+				setMadeDifference(list.get(i),a);
 			} else if(a==4) {
 				summaryObject.setCoveredValue(list.get(i));
 			} else if(a==5) {
 				summaryObject.setRecomendation(list.get(i));
 			} else if(a==6) {
-				if(list.get(i).equalsIgnoreCase("M")) summaryObject.setGender("he");
-				if(list.get(i).equalsIgnoreCase("F")) summaryObject.setGender("she");
+				setGender(list.get(i),a);
 			}
 		}
-		
+		logger.info("List to object is ... ");
 		return summaryObject;
 	}
+	
+	private void setMadeDifference(String in, int a) {
+		if(in.equals("1")) summaryObject.setMadeADifference(true);
+		if(in.equals("0")) summaryObject.setMadeADifference(false);
+	}
 
+	private void setGender(String in, int a) {
+		if(in.equalsIgnoreCase("M")) summaryObject.setGender("he");
+		if(in.equalsIgnoreCase("F")) summaryObject.setGender("she");
+	}
 }
