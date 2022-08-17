043b7301037efa3a23d978043545be1d9f16d4b3
andregoulbourne <andre.goulbourne2@gmail.com>
2022-08-17 12:42:19 -0400

    WriteMyClientReportForm-6: implemented a way to delete and add a summary.

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams} from '@angular/common/http';
 import { AgGridAngular } from 'ag-grid-angular';
 import { CellValueChangedEvent } from 'ag-grid-community';
 
@@ -36,13 +36,19 @@ export class AppComponent {
   tutorComment:any;
 
   writeComment() {
-    const selectedNodes = this.agGrid?.api.getSelectedNodes();
-    const selectedData = selectedNodes?.map(node => node.data);
+    let selectedData = this.getSelectedSummary();
     
     this._http.post<any>('http://localhost:8081/summarys/writeAComment.do', selectedData/*JSON.stringify(selectedData),  {headers: {'Content-Type':'application/json'}}*/)
     .subscribe((data) => {
       this.tutorComment= data.comment;
     });
+  }
+  
+  private getSelectedSummary() {
+	const selectedNodes = this.agGrid?.api.getSelectedNodes();
+    const selectedData = selectedNodes?.map(node => node.data);
+    
+    return selectedData;
   }
 
   updateSummary(params: CellValueChangedEvent){
@@ -50,10 +56,52 @@ export class AppComponent {
     const updatedSummary = params.data;
     this._http.post<any>('http://localhost:8081/summarys/updateSummary.do',updatedSummary)
       .subscribe((data)=>{
-        console.log(updatedSummary);
         if(data.msg=="SUCCESS") alert('Update is successful ...')
         else alert('Update has failed ...')
       })
+  }
+  
+  addDefaultInitializedSummary(){
+	console.log("Adding Default Summary ...")
+	const summary = {id: this.getMaxIdNumber()+1, student: '', status: '', madeADifference: true, coveredValue: '', recomendation: '', gender:'F'};
+	
+	this._http.post<any>('http://localhost:8081/summarys',summary)
+      .subscribe((data)=>{
+        if(data.msg=="SUCCESS"){
+			alert('Insert is successful ...')
+			this.ngOnInit();
+        } else alert('Insert has failed ...')
+      })
+  }
+  
+  private getMaxIdNumber(){
+	let maxId = 0;
+	for (let summary of this.rowData) {
+	  let currentId = parseInt(summary.id);
+	  
+	  if(currentId > maxId) 
+	  	maxId = currentId; 
+	}
+	
+	return maxId;
+  }
+  
+  deleteSummary(){
+	console.log("Deleting Summary ...")
+	
+	let selectedData = this.getSelectedSummary();
+	
+	selectedData?.forEach( (summary: any) => {
+		let httpParams = new HttpParams();
+		httpParams = httpParams.set('id', summary.id);
+		this._http.post<any>('http://localhost:8081/summarys/deleteSummary.do', httpParams)
+	      .subscribe((data)=>{
+	        if(data.msg =="SUCCESS"){
+				alert('Delete is successful ...')
+				this.ngOnInit();
+	        } else alert('Delete has failed ...')
+	      })
+    });
   }
 
 }
