83430b7046bf4aedbd22acafc94bc9772912acf0
andregoulbourne <andre.goulbourne2@gmail.com>
2022-12-29 10:31:50 -0500

    WriteMyClientReportForms-11: removing all setReflectionTestsUtil and setting up the mocks at the beginning of the test for each test class, and only using mockito for different concerns in the test

--- src/test/java/com/andre/service/SummaryServiceTest.java
+++ src/test/java/com/andre/service/SummaryServiceTest.java
@@ -7,8 +7,10 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.mockito.MockitoAnnotations;
 
 import com.andre.constants.Constants;
 import com.andre.dao.SummaryDao;
@@ -16,45 +18,44 @@ import com.andre.model.SummaryVO;
 
 class SummaryServiceTest {
 	
-	private SummaryService service = new SummaryService();
+	@InjectMocks
+	private SummaryService service;
+	
+	@Mock
 	private SummaryDao dao;
+	
+	private static final String ID = "1";
 	
 	@BeforeEach
 	void setup() {
-		dao = Mockito.mock(SummaryDao.class);
+		MockitoAnnotations.openMocks(this);
 	}
 	
 	@Test
 	void testGetAllSummary(){
 		List<SummaryVO> rs = new ArrayList<>();
 		Mockito.when(dao.readCSVFile(null, Constants.ALL)).thenReturn(rs);
-		ReflectionTestUtils.setField(service,"dao", dao); 
 		
 		assertEquals(rs,service.getAllSummary());
 	}
 	
-	private static final String id = "1";
+	
 	
 	@Test
 	void testGetASummary() {
 		SummaryVO rs = new SummaryVO();
-		Mockito.when(dao.readCSVFileSingleEntry(id)).thenReturn(rs);
-		ReflectionTestUtils.setField(service,"dao", dao); 
+		Mockito.when(dao.readCSVFileSingleEntry(ID)).thenReturn(rs);
 		
-		assertEquals(rs,service.getASummary(id));
+		assertEquals(rs,service.getASummary(ID));
 	}
 	
 	private static final SummaryVO o = new SummaryVO();
 	
 	@Test
 	void testUpdateSummary() {
-		
-		ReflectionTestUtils.setField(service,"dao", dao);
-		
 		assertEquals(0,service.updateSummary(o));
 		
 		Mockito.when(dao.csvFileEntry(o, Constants.UPDATE)).thenReturn(1);
-		ReflectionTestUtils.setField(service,"dao", dao); 
 		
 		assertEquals(1,service.updateSummary(o));
 	}
@@ -62,7 +63,6 @@ class SummaryServiceTest {
 	@Test
 	void testAddSummary() {
 		Mockito.when(dao.csvFileEntry(o, Constants.ADD)).thenReturn(1);
-		ReflectionTestUtils.setField(service,"dao", dao); 
 		
 		assertEquals(1,service.addSummary(o));
 	}
@@ -70,7 +70,6 @@ class SummaryServiceTest {
 	@Test
 	void testDeleteSummary() {
 		Mockito.when(dao.csvFileEntry(o, Constants.DELETE)).thenReturn(1);
-		ReflectionTestUtils.setField(service,"dao", dao); 
 		
 		assertEquals(1,service.deleteSummary(o));
 	}
--- src/test/java/com/andre/service/WriteCommentServiceTest.java
+++ src/test/java/com/andre/service/WriteCommentServiceTest.java
@@ -6,15 +6,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.andre.model.SummaryVO;
 
 class WriteCommentServiceTest {
 	
+	private WriteCommentService service;
+	
+	@BeforeEach
+	void setup() {
+		service = new WriteCommentService();
+	}
+	
 	@Test
 	void testWriteComment_returnTutorCommnentString() {
-		WriteCommentService service = new WriteCommentService();
 		String expected = "On a scale from 1-3, Andre confidence level with the material was at a 3 at the beginning of the session.\r\n"
 				+ "\r\n"
 				+ "In the session we worked on finding critical numbers and increasing decreasing test.\r\n"
