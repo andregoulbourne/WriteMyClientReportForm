a188cb92e70c0f34852582875899a56e53913259
Andre Goulbourne <andre.goulbourne2@gmail.com>
2024-05-05 19:42:49 -0400

    adding test cases for the selenium test suite

--- src/test/java/com/andre/selenium/SeleniumUtil.java
+++ src/test/java/com/andre/selenium/SeleniumUtil.java
@@ -1,73 +1,92 @@
 package com.andre.selenium;
 
+import com.andre.WriteMyClientReportForm;
+import com.andre.exceptions.ChromeDriverMissingException;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.log4j.Logger;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-import com.andre.exceptions.ChromeDriverMissingException;
+import java.util.concurrent.TimeUnit;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SeleniumUtil {
-	
-	private static Logger logger = Logger.getLogger(SeleniumUtil.class);
 
-	private static final String OS_NAME = "os.name";
-	
-	private static final String CHROME_DRIVER_PATH_LINUX = "./selenium/chromedriver";
-	private static final String CHROME_DRIVER_PATH_WINDOWS = "./selenium/chromedriver.exe";
-	
-	@LocalServerPort
-	int randomServerPort;
+	private static final Logger logger = Logger.getLogger(SeleniumUtil.class);
+
+	private static SeleniumUtil instance;
 
 	protected static WebDriver driver;
 
 	@BeforeEach
-	void setup() throws ChromeDriverMissingException {
+	void setupSelenium() throws ChromeDriverMissingException {
 		try{
-			driver = getWebDriverInstance();
-			driver.manage().window().maximize();
-			System.out.println(randomServerPort);
-			driver.get("http://localhost:" + randomServerPort + "/");
+			if(driver == null) {
+				startApplication();
 
+				driver = getWebDriverInstance();
+				driver.manage().window().maximize();
+				driver.get("http://localhost:" + 8081 + "/");
+				waitSeconds(5);
+			}
+			waitSeconds(3);
 		} catch(Exception e){
 			logger.error("Setting up in selenium util, "+ e.getMessage());
 		}
 	}
 
 	@AfterEach
-	void tearDown() {
+	void tearDownSelenium() {
 		try{
-			driver.quit();
+			//driver.quit();
 		} catch(Exception e){
 			logger.error("Tearing Down in selenium util, "+ e.getMessage());
+		}
+	}
+
+	protected void waitSeconds(int seconds) throws InterruptedException {
+		long miliSeconds = seconds * 1000L;
+
+		synchronized (this){
+			wait(miliSeconds);
 		}
 	}
 
 	private static WebDriver getWebDriverInstance() throws ChromeDriverMissingException {
 		try{
 			if (driver == null) {
-				String os = System.getProperty(OS_NAME);
-				if(os.equals("Linux")) {
-					System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH_LINUX);
-				}else if(os.contains("Windows")) {
-					System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH_WINDOWS);
-				}else {
-					throw new ChromeDriverMissingException();
-				}
-				
+				WebDriverManager.chromedriver().setup();
+				ChromeOptions options = new ChromeOptions();
 				driver = new ChromeDriver();
+				driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);;
 			}
 		} catch(Exception e){
-			logger.error("An execption occurred while setting chrome web driver , "+ e.getMessage());
+			logger.error("An exception occurred while setting chrome web driver , "+ e.getMessage());
+			throw new ChromeDriverMissingException();
 		}
 
 		return driver;
 	}
 
+	private void startApplication() throws InterruptedException {
+		String[] args = {"-Dserver.port=8081", "-DskipTests"};
+		WriteMyClientReportForm.main(args);
+		waitSeconds(5);
+	}
+
+	protected void closeAlert() throws InterruptedException {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			logger.info("Alert data: " + alertText);
+			alert.accept();
+			waitSeconds(2);
+		} catch(NoAlertPresentException e){
+			logger.error("No such alert exist");
+		}
+	}
 }
